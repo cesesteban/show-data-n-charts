89607975496b2438b1006c5cb2e200cff49f57fd
ces-esteban <ecespedes@kubikware.com>
2022-05-09 21:12:31 -0300

    material icons

--- src/components/CardView/CardViewBinance.js
+++ src/components/CardView/CardViewBinance.js
@@ -1,42 +1,38 @@
-import React, { useEffect, useMemo, useState } from "react";
-import Grid from "@mui/material/Grid";
-import CardDetailBinance from "../CardDetail/CardDetailBinance";
-import styles from "./cardView.module.css";
+import React, { useEffect, useMemo, useState } from "react"
+import Grid from "@mui/material/Grid"
+import CardDetailBinance from "../CardDetail/CardDetailBinance"
+import styles from "./cardView.module.css"
 
 function CardViewBinance({ dark }) {
-  const [tickers, setTickers] = useState();
+ const [tickers, setTickers] = useState()
 
-  const getPremiumIndex = async () => {
-    let data = await fetch(`https://dapi.binance.com/dapi/v1/premiumIndex`, {
-      method: "GET",
-    });
-    data = await data.json();
-    data = data.filter((pair) => pair.symbol.includes("0"));
-    data = data.map((data) => data.symbol);
-    data = data.sort();
-    data = data.map((pair) => {
-      return { coin: pair, spot: pair.split("_")[0] + "T" };
-    });
-    setTickers(data);
-  };
+ const getPremiumIndex = async () => {
+  let data = await fetch(`https://dapi.binance.com/dapi/v1/premiumIndex`, {
+   method: "GET",
+  })
+  data = await data.json()
+  data = data.filter((pair) => pair.symbol.includes("0"))
+  data = data.map((data) => data.symbol)
+  data = data.sort()
+  data = data.map((pair) => {
+   return { coin: pair, spot: pair.split("_")[0] + "T" }
+  })
+  setTickers(data)
+ }
 
-  useEffect(() => {
-    getPremiumIndex();
-  }, []);
+ useEffect(() => {
+  if (!tickers) getPremiumIndex()
+ }, [])
 
-  return (
-    <Grid className={styles.root} container spacing={2}>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={2}>
-          {tickers &&
-            tickers[0] &&
-            tickers.map((value, index) => (
-              <CardDetailBinance dark={dark} value={value} index={index} />
-            ))}
-        </Grid>
-      </Grid>
+ return (
+  <Grid className={styles.root} container spacing={2}>
+   <Grid item xs={12}>
+    <Grid container justifyContent="center" spacing={2}>
+     {tickers && tickers[0] && tickers.map((value, index) => <CardDetailBinance dark={dark} value={value} index={index} />)}
     </Grid>
-  );
+   </Grid>
+  </Grid>
+ )
 }
 
-export default CardViewBinance;
+export default CardViewBinance
